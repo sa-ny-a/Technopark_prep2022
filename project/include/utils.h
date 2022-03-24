cfd811e9977febbc4ed2dc9bffc2f40d240213c1
Alexandra <sasha.krasnova65@gmail.com>
2022-03-24 15:46:04 +0300

    Выделила функции в отдельный файл

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,6 +1,23 @@
-#define UTILS_H
-#ifndef UTILS_H
+#define PROJECT_INCLUDE_UTILS_H
+#ifndef PROJECT_INCLUDE_UTILS_H
 
-#define filename "transaction.dat";
+#define filename "transaction.dat"
 
-#endif //UTILS_H
+struct masterRecord {
+	int Number;
+	char Name[20];
+	char Surname[20];
+	char addres[30];
+	char TelNumber[15];
+	double indebtedness;
+	double credit_limit;
+	double cash_payments;
+};
+
+typedef  struct  masterRecord Data;
+
+void masterWrite(FILE *ofPTR, Data Client);
+void transactionWrite(FILE *ofPTR, Data transfer);
+void blackRecord(FILE *ofPTR, FILE  *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer);
+
+#endif // PROJECT_INCLUDE_UTILS_H
