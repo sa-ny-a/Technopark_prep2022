a3081e216cb169516ac3ac888251b39f5d67728e
Alexandra <sasha.krasnova65@gmail.com>
2022-03-24 17:33:37 +0300

    Нужно исправить ошибки cpplint

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,23 +1,25 @@
-#ifndef PROJECT_INCLUDE_UTILS_H
-#define PROJECT_INCLUDE_UTILS_H
+#ifndef PROJECT_INCLUDE_UTILS_H_
+#define PROJECT_INCLUDE_UTILS_H_
 
 #define filename_blackrecord "blackrecord.dat"
 #define filename_record "record.dat"
 #define filename_transaction "transaction.dat"
 
+#include <stdio.h>
+
 typedef struct masterRecord {
-	int Number;
-	char Name[20];
-	char Surname[20];
-	char addres[30];
-	char TelNumber[15];
-	double indebtedness;
-	double credit_limit;
-	double cash_payments;
+    int Number;
+    char Name[20];
+    char Surname[20];
+    char addres[30];
+    char TelNumber[15];
+    double indebtedness;
+    double credit_limit;
+    double cash_payments;
 } Data;
 
 void masterWrite(FILE *ofPTR, Data Client);
 void transactionWrite(FILE *ofPTR, Data transfer);
 void blackRecord(FILE *ofPTR, FILE  *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer);
 
-#endif  // PROJECT_INCLUDE_UTILS_H
+#endif  // PROJECT_INCLUDE_UTILS_H_
--- project/src/utils.c
+++ project/src/utils.c
@@ -14,7 +14,7 @@ void masterWrite(FILE *ofPTR, Data Client)
 			"6 Client indebtedness: ",
 			"7 Client credit limit: ",
 			"8 Client cash payments: " );
-	while(scanf("%d%s%s%s%s%lf%lf%lf", &Client.Number, Client.Name, Client.Surname, Client.addres,
+	while(scanf("%99d%99s%99s%99s%99s%99lf%99lf%99lf", &Client.Number, Client.Name, Client.Surname, Client.addres,
                 Client.TelNumber, &Client.indebtedness, &Client.credit_limit, &Client.cash_payments) != -1)
 	{
 		fprintf(ofPTR, "%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n", Client.Number, Client.Name,
@@ -47,7 +47,7 @@ void transactionWrite(FILE *ofPTR, Data transfer)
 
 void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer)
 {
-	while(fscanf(ofPTR, "%d%s%s%s%s%lf%lf%lf", &client_data.Number, client_data.Name,
+	while(fscanf(ofPTR, "%99d%99s%99s%99s%99s%99lf%99lf%99lf", &client_data.Number, client_data.Name,
                   client_data.Surname, client_data.addres, client_data.TelNumber, &client_data.indebtedness,
                   &client_data.credit_limit, &client_data.cash_payments) != -1)
 	{
