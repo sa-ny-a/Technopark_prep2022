9be16c1b3ce5e8341acde5c6c8647fe69a008b00
Alexandra <sasha.krasnova65@gmail.com>
2022-04-01 01:06:45 +0300

    Должны работать тесты для каждой функции

---
+++ project/include/test.h
@@ -0,0 +1,14 @@
+#ifndef PROJECT_INCLUDE_TEST_H_
+#define PROJECT_INCLUDE_TEST_H_
+
+#define filename_updaterecord_test "updaterecord_test.dat"
+#define filename_record_test "record_test.dat"
+#define filename_transaction_test "transaction_test.dat"
+
+void general_test();
+
+int record_test();
+int transaction_test();
+int updaterecord_test();
+
+#endif  // PROJECT_INCLUDE_TEST_H_
