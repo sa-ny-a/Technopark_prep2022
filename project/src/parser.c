0d964ef5cc848333769446da996bf5005056fc8e
Alexandra <sasha.krasnova65@gmail.com>
2022-04-18 09:41:13 +0300

    Работаю над прохождением тестов

--- project/src/parser.c
+++ project/src/parser.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "../include/parser.h"
+
+#define SIZE_OF_ARRAY 1024
 
 typedef struct Message {
     char* nm_from;
@@ -14,9 +17,9 @@ typedef struct Message {
 
 static Data* init() {
     Data* dt = malloc(sizeof(Data));
-    dt->nm_from = (char*)malloc(128);
-    dt->nm_to = (char*)malloc(128);
-    dt->date = (char*)malloc(128);
+    dt->nm_from = (char*)malloc(SIZE_OF_ARRAY);
+    dt->nm_to = (char*)malloc(SIZE_OF_ARRAY);
+    dt->date = (char*)malloc(SIZE_OF_ARRAY);
     return dt;
 }
 
@@ -26,13 +29,13 @@ static int get_name_from(const char* path, Data* dt) {
         printf("Not open: ");
         return 0;
     } else {
-        char str[128];
+        char str[SIZE_OF_ARRAY];
         while (!feof(email)) {
-            fgets(str, 128, email);
+            fgets(str, SIZE_OF_ARRAY, email);
             if(strlen(str) > 10) {
                 if (*(str) == 'F' && *(str + 1) == 'r' && *(str + 2) == 'o' && *(str + 3) == 'm') {
                     for (size_t i = 6; i < strlen(str); i++) {
-                        if (*(str + i) != '\n') {
+                        if (*(str + i) != '\n' && *(str + i) != '\r') {
                             dt->nm_from = strncat(dt->nm_from, &*(str + i), 1);
                         } else {
                             break;
@@ -54,25 +57,40 @@ static int get_name_to(const char* path, Data* dt) {
         printf("Not open: ");
         return 0;
     } else {
-        char str[128];
+        char str[SIZE_OF_ARRAY];
+        int MKR_TO = 0;
         while (!feof(email)) {
-            fgets(str, 128, email);
+            fgets(str, SIZE_OF_ARRAY, email);
             if(strlen(str) > 10) {
-                if (*(str) == 'T' && *(str + 1) == 'o') {
+                if ((*(str) == 'T' && *(str + 1) == 'o') && MKR_TO == 0) {
                     for (size_t i = 4; i < strlen(str); i++) {
-                        if (*(str + i) != '\n') {
+                        if (*(str + i) != '\n' && *(str + i) != '\r') {
                             dt->nm_to = strncat(dt->nm_to, &*(str + i), 1);
                         } else {
                             break;
                         }
                     }
-                    fclose(email);
-                    return 1;
+                    MKR_TO = 1;
+                } else {
+                    if (MKR_TO == 1) {
+                        if (*(str) == ' ') {
+                            for (size_t i = 0; i < strlen(str); i++) {
+                                if (*(str + i) != '\n' && *(str + i) != '\r') {
+                                    dt->nm_to = strncat(dt->nm_to, &*(str + i), 1);
+                                } else {
+                                    break;
+                                }
+                            }
+                        } else {
+                            fclose(email);
+                            return 1;
+                        }
+                    }
                 }
             }
         }
         fclose(email);
-        return 0;
+        return 1;
     }
 }
 
@@ -82,13 +100,13 @@ static int get_date(const char* path, Data* dt) {
         printf("Not open: ");
         return 0;
     } else {
-        char str[128];
+        char str[SIZE_OF_ARRAY];
         while (!feof(email)) {
-            fgets(str, 128, email);
+            fgets(str, SIZE_OF_ARRAY, email);
             if(strlen(str) > 10) {
                 if (*(str) == 'D' && *(str + 1) == 'a' && *(str + 2) == 't' && *(str + 3) == 'e') {
                     for (size_t i = 6; i < strlen(str); i++) {
-                        if (*(str + i) != '\n') {
+                        if (*(str + i) != '\n' && *(str + i) != '\r') {
                             dt->date = strncat(dt->date, &*(str + i), 1);
                         } else {
                             break;
@@ -129,18 +147,17 @@ static int get_score(const char* path, Data* dt) {
         printf("Not open: ");
         return 0;
     } else {
-        char str[128];
+        char str[SIZE_OF_ARRAY];
         int MKR_TXT = 1, MKR_KEY = 0;
         dt->score = 0;
-        char *buf_key = (char *) malloc(128);  // Если напрямую записывать в ключ, то возникает ошибка
+        char *buf_key = (char *) malloc(SIZE_OF_ARRAY);  // Если напрямую записывать в ключ, то возникает ошибка
         while (!feof(email)) {
-            fgets(str, 128, email);
+            fgets(str, SIZE_OF_ARRAY, email);
             if (strlen(str) > 10) {
-                char *key = (char *) malloc(128);
+                char *key = (char *) malloc(SIZE_OF_ARRAY);
                 if (MKR_TXT == 1) {  // Проверка на наличие инфы
                     if (*(str) == 'C' && *(str + 1) == 'o' && *(str + 2) == 'n' && *(str + 3) == 't'
                         && *(str + 4) == 'e' && *(str + 5) == 'n' && *(str + 6) == 't') {
-                        dt->score = 1;
                         MKR_TXT = 0;
                         MKR_KEY = get_key(str, key);  // Ключ еще на этой строке
                         buf_key = strcpy(buf_key, key);
@@ -151,7 +168,7 @@ static int get_score(const char* path, Data* dt) {
                         buf_key = strcpy(buf_key, key);
                     } else  {
                         if (*(str) == '-' && *(str + 1) == '-') {
-                            char *buf = (char *) malloc(128);
+                            char *buf = (char *) malloc(SIZE_OF_ARRAY);
 
                             for (size_t i = 2; i < strlen(str); i++) {
                                 if (*(str + i) != '\n') {
@@ -160,13 +177,16 @@ static int get_score(const char* path, Data* dt) {
                                     break;
                                 }
                             }
-                            if (strlen(buf_key) == strlen(buf)) {
+                            if (strcmp(buf, buf_key) == 0) {
                                 dt->score += 1;
                             }
                         }
                     }
                 }
             }
+        }
+        if ((MKR_KEY == 0 && MKR_TXT == 0) || (MKR_KEY == 0 && MKR_TXT == 1)) {
+            dt->score = 1;
         }
         fclose(email);
         return 1;
@@ -183,7 +203,7 @@ int parser(const char* path) {
             if (get_date(path, info) == 1) {
                 printf("|%s", info->date);
                 if (get_score(path, info) == 1) {
-                    printf("|%d\n", info->score - 1);
+                    printf("|%d\n", info->score);
                     return 1;
                 } else {
                     printf("\nNo score: ");
