7cb16a22559c4730b64039e0500c523f57d20fe5
Alexandra <sasha.krasnova65@gmail.com>
2022-04-19 12:49:17 +0300

    Убрала вложенность по возможности, добавила тесты

--- project/src/parser.c
+++ project/src/parser.c
@@ -25,66 +25,64 @@ void check_string_on_annons_no_back(const char* path, const char* control) {
     if (ptr == NULL) {
         printf("Not open: ");
         return;
-    } else {
-        char *buf = (char*)calloc(SIZE_OF_ARRAY, sizeof(char));
-        char str[SIZE_OF_ARRAY];
-        int MRK_FIRST_STR = ZERO_CONST;  // Контроль первой строки
-        int MRK_STR_FROM = ZERO_CONST;  //  1 когда встретим заголовок
-        int MRK_NEWSTR_FROM = ZERO_CONST;  // Если сообщение более чем на 1 строку
-        while (!feof(ptr)) {
-            fgets(str, SIZE_OF_ARRAY, ptr);
-            if (strlen(str) > STR_INFO_SIZE) {
-                 if (MRK_STR_FROM == ZERO_CONST) {  // Если заголовок не найдет выполняется эта часть кода
-                    if (check_str_ctl(str, control) == strlen(control)) {
-                        clear_first_str(str, control);  // Очищаем начала строки, чтобы ошибок потом не было
-                        MRK_STR_FROM = ONE_CONST;
-                        MRK_FIRST_STR = ONE_CONST;
-                    }
+    }
+    char *buf = (char*)calloc(SIZE_OF_ARRAY, sizeof(char));
+    char str[SIZE_OF_ARRAY];
+    int MRK_FIRST_STR = ZERO_CONST;  // Контроль первой строки
+    int MRK_STR_FROM = ZERO_CONST;  //  1 когда встретим заголовок
+    int MRK_NEWSTR_FROM = ZERO_CONST;  // Если сообщение более чем на 1 строку
+    while (!feof(ptr)) {
+        fgets(str, SIZE_OF_ARRAY, ptr);
+        if (strlen(str) > STR_INFO_SIZE) {
+            if (MRK_STR_FROM == ZERO_CONST) {  // Если заголовок не найдет выполняется эта часть кода
+                if (check_str_ctl(str, control) == strlen(control)) {
+                    clear_first_str(str, control);  // Очищаем начала строки, чтобы ошибок потом не было
+                    MRK_STR_FROM = ONE_CONST;
+                    MRK_FIRST_STR = ONE_CONST;
                 }
-                if (MRK_STR_FROM == ONE_CONST) {
-                    if (MRK_NEWSTR_FROM != ONE_CONST) {
-                        for (size_t i = 0; i < strlen(str); i++) {
-                            if (i == ZERO_CONST && *(str) == ' ' && MRK_FIRST_STR == 1) {
-                                MRK_FIRST_STR = ZERO_CONST;
-                            } else {
-                                if (*(str + i) != '\n' && *(str + i) != '\r' && *(str + i) != '\0') {
-                                    if (*(str + i) == ',') {
-                                        printf("%s", buf);
-                                        buf = strncpy(buf, "", strlen(buf));
-                                    }
-                                    buf = strncat(buf, &*(str + i), 1);
-                                } else {
-                                    MRK_NEWSTR_FROM = ONE_CONST;
-                                    break;
-                                }
+            }
+            if (MRK_STR_FROM == ONE_CONST) {
+                if (MRK_NEWSTR_FROM != ONE_CONST) {
+                    for (size_t i = 0; i < strlen(str); i++) {
+                        if (i == ZERO_CONST && *(str) == ' ' && MRK_FIRST_STR == 1) {
+                            MRK_FIRST_STR = ZERO_CONST;
                             }
+                        if (*(str + i) != '\n' && *(str + i) != '\r' && *(str + i) != '\0') {
+                            if (*(str + i) == ',') {
+                                printf("%s", buf);
+                                buf = strncpy(buf, "", strlen(buf));
+                            }
+                            buf = strncat(buf, &*(str + i), 1);
+                        } else {
+                            MRK_NEWSTR_FROM = ONE_CONST;
+                            break;
+                            }
+                    }
+                } else {
+                    if (*(str) == ' ') {
+                        for (size_t i = 0; i < strlen(str); i++) {
+                            if (*(str + i) != '\n' && *(str + i) != '\r' && *(str + i) != '\0') {
+                                if (*(str + i) == ',') {
+                                    printf("%s", buf);
+                                    buf = strncpy(buf, "", strlen(buf));
+                                }
+                                buf = strncat(buf, &*(str + i), 1);
+                            } else {
+                                break;
+                                }
                         }
                     } else {
-                        if (*(str) == ' ') {
-                            for (size_t i = 0; i < strlen(str); i++) {
-                                if (*(str + i) != '\n' && *(str + i) != '\r' && *(str + i) != '\0') {
-                                    if (*(str + i) == ',') {
-                                        printf("%s", buf);
-                                        buf = strncpy(buf, "", strlen(buf));
-                                    }
-                                    buf = strncat(buf, &*(str + i), 1);
-                                } else {
-                                    break;
-                                }
-                            }
-                        } else {
-                            printf("%s", buf);
-                            fclose(ptr);
-                            free(buf);
-                            return;
-                        }
+                        printf("%s", buf);
+                        fclose(ptr);
+                        free(buf);
+                        return;
                     }
                 }
             }
         }
-        free(buf);
-        fclose(ptr);
     }
+    free(buf);
+    fclose(ptr);
 }
 
 
@@ -93,43 +91,42 @@ int check_string_on_annons(const char* path, const char* control, char* back) {
     if (ptr == NULL) {
         printf("Not open: ");
         return ZERO_CONST;
-    } else {
-        char str[SIZE_OF_ARRAY];
-        int MRK_STR_FROM = ZERO_CONST;  // Маркер контроля заголовка, равен 1, когда встретим заголовок
-        int MRK_NEWSTR_FROM = ZERO_CONST;  // Маркер пробела, если сообщение более, чем на 1 строку
-        while (!feof(ptr)) {
-            fgets(str, SIZE_OF_ARRAY, ptr);
-            if (strlen(str) > STR_INFO_SIZE) {
-                if (MRK_STR_FROM == ZERO_CONST) {
-                    if (check_str_ctl(str, control) == strlen(control)) {
-                        MRK_STR_FROM = ONE_CONST;
-                    }
+    }
+    char str[SIZE_OF_ARRAY];
+    int MRK_STR_FROM = ZERO_CONST;  // Маркер контроля заголовка, равен 1, когда встретим заголовок
+    int MRK_NEWSTR_FROM = ZERO_CONST;  // Маркер пробела, если сообщение более, чем на 1 строку
+    while (!feof(ptr)) {
+        fgets(str, SIZE_OF_ARRAY, ptr);
+        if (strlen(str) > STR_INFO_SIZE) {
+            if (MRK_STR_FROM == ZERO_CONST) {
+                if (check_str_ctl(str, control) == strlen(control)) {
+                    MRK_STR_FROM = ONE_CONST;
                 }
-                if (MRK_STR_FROM == ONE_CONST) {
-                    if (MRK_NEWSTR_FROM != ONE_CONST) {
-                        for (size_t i = strlen(control); i < strlen(str); i++) {
-                            if (i != strlen(control) && *(str + i) != ' ') {
-                                if ((*(str + i) != '\n' && *(str + i) != '\r')) {
-                                    back = strncat(back, &*(str + i), 1);
-                                } else {
-                                    break;
+            }
+            if (MRK_STR_FROM == ONE_CONST) {
+                if (MRK_NEWSTR_FROM != ONE_CONST) {
+                    for (size_t i = strlen(control); i < strlen(str); i++) {
+                        if (i != strlen(control) && *(str + i) != ' ') {
+                            if ((*(str + i) != '\n' && *(str + i) != '\r')) {
+                                back = strncat(back, &*(str + i), 1);
+                            } else {
+                                break;
                                 }
-                            }
                         }
-                        MRK_NEWSTR_FROM = ONE_CONST;
+                    }
+                    MRK_NEWSTR_FROM = ONE_CONST;
+                } else {
+                    if (*(str) == ' ') {
+                        for (size_t i = 0; i < strlen(str); i++) {
+                            if (*(str + i) != '\n' && *(str + i) != '\r') {
+                                back = strncat(back, &*(str + i), 1);
+                            } else {
+                                break;
+                                }
+                        }
                     } else {
-                        if (*(str) == ' ') {
-                            for (size_t i = 0; i < strlen(str); i++) {
-                                if (*(str + i) != '\n' && *(str + i) != '\r') {
-                                    back = strncat(back, &*(str + i), 1);
-                                } else {
-                                    break;
-                                }
-                            }
-                        } else {
-                            fclose(ptr);
-                            return ONE_CONST;
-                        }
+                        fclose(ptr);
+                        return ONE_CONST;
                     }
                 }
             }
@@ -186,9 +183,8 @@ static int get_key_in_txt(const char* path, char* key) {
         memcpy(key, buf, strlen(buf));
         free(buf);
         return ONE_CONST;
-    } else {
-        return ZERO_CONST;
     }
+    return ZERO_CONST;
 }
 
 
@@ -211,38 +207,36 @@ void get_score(const char* path) {
             free(key);
             free(buf);
             return;
-        } else {
-            char str[SIZE_OF_ARRAY];
-            int score = ZERO_CONST;
-            while (!feof(email)) {
-                fgets(str, SIZE_OF_ARRAY, email);
-                if (strlen(str) > 2) {
-                    if (*(str) == '-' && *(str + 1) == '-') {
-                        buf = strncpy(buf, "", 1);
+        }
+        char str[SIZE_OF_ARRAY];
+        int score = ZERO_CONST;
+        while (!feof(email)) {
+            fgets(str, SIZE_OF_ARRAY, email);
+            if (strlen(str) > 2) {
+                if (*(str) == '-' && *(str + 1) == '-') {
+                    buf = strncpy(buf, "", 1);
 
-                        for (size_t i = 2; i < strlen(str); i++) {
-                            if (*(str + i) != '\n' && *(str + i) != '\r') {
-                                buf = strncat(buf, &*(str + i), 1);
-                            } else {
-                                break;
+                    for (size_t i = 2; i < strlen(str); i++) {
+                        if (*(str + i) != '\n' && *(str + i) != '\r') {
+                            buf = strncat(buf, &*(str + i), 1);
+                        } else {
+                            break;
                             }
-                        }
-                        if (strcmp(buf, key) == ZERO_CONST) {
-                            score += 1;
-                        }
+                    }
+                    if (strcmp(buf, key) == ZERO_CONST) {
+                        score += 1;
                     }
                 }
             }
-            free(key);
-            free(buf);
-            fclose(email);
-            printf("%d\n", score);
-            return;
         }
-    } else {
         free(key);
         free(buf);
+        fclose(email);
+        printf("%d\n", score);
+        return;
     }
+    free(key);
+    free(buf);
     if ((MRK_KEY == ZERO_CONST && MRK_STR_CONTENT == ZERO_CONST)
         || (MRK_KEY == ZERO_CONST && MRK_STR_CONTENT == ONE_CONST)) {
         printf("1\n");
